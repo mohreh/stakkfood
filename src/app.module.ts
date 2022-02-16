521aebc2f17bb9b7774c3780a1fdc0906261d67a
Mohammad Reza Hassani <reza_hassani_@outlook.com>
2022-02-17 03:04:24 +0330

    add configuration and .env stuff

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('development', 'test', 'production'),
+      }),
+      isGlobal: true,
+      envFilePath: [`.env.${process.env.NODE_ENV}`],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
