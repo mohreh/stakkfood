cba2a33039eae7f33e1f33a3ca43a368991c8d5d
Mohammad Reza Hassani <reza_hassani_@outlook.com>
2022-02-17 03:29:12 +0330

    add postgres and typeorm connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -10,10 +11,31 @@ import { UsersModule } from './users/users.module';
     UsersModule,
     ConfigModule.forRoot({
       validationSchema: Joi.object({
-        NODE_ENV: Joi.string().valid('development', 'test', 'production'),
+        PORT: Joi.string(),
+        NODE_ENV: Joi.string()
+          .required()
+          .valid('development', 'test', 'production'),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
       }),
       isGlobal: true,
       envFilePath: [`.env.${process.env.NODE_ENV}`],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'postgres',
+          DB_HOST: config.get('DB_HOST'),
+          DB_PORT: +config.get('DB_PORT'),
+          DB_USERNAME: config.get('DB_USERNAME'),
+          DB_PASSWORD: config.get('DB_PASSWORD'),
+          DB_NAME: config.get('DB_NAME'),
+        };
+      },
     }),
   ],
   controllers: [AppController],
