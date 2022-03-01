155c577a2369a5be944fbb2f0a19bf01af07cdc7
Mohammad Reza Hassani <reza_hassani_@outlook.com>
2022-03-01 10:07:04 +0330

    start with testin, this is my first try in testing, I will make it better.

--- server/auth/auth.controller.spec.ts
+++ server/auth/auth.controller.spec.ts
@@ -1,12 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Request } from 'express';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { Role } from './enum/role.enum';
+
+const id = 'randomId';
+const token = 'token';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
   beforeEach(async () => {
+    const mockAuthService = {
+      sendAuthenticationCode: jest.fn((_phoneNumber) => id),
+      login: jest.fn((_id) => ({
+        access_token: token,
+      })),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: mockAuthService,
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
@@ -14,5 +33,21 @@ describe('AuthController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return a request id', async () => {
+    expect(
+      await controller.sendAuthenticationCode({ phoneNumber: '+9812345' }),
+    ).toEqual({ reqId: id });
+  });
+
+  it('should return an access_token', () => {
+    expect(
+      controller.verifyUser({
+        user: { id, role: Role.Client },
+      } as Request),
+    ).toEqual({
+      access_token: token,
+    });
   });
 });
--- server/auth/auth.service.spec.ts
+++ server/auth/auth.service.spec.ts
@@ -1,12 +1,67 @@
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { plainToClass } from 'class-transformer';
+import { idGenerator } from '../common/nanoid';
+import { SmsService } from '../sms/sms.service';
+import { UsersService } from '../users/users.service';
 import { AuthService } from './auth.service';
+import { AuthCode } from './entities/auth-code.entity';
 
 describe('AuthService', () => {
   let service: AuthService;
 
+  const id = idGenerator();
+  const phoneNumber = idGenerator(10);
+  const pin = idGenerator(5);
+
+  const mockAuthCodeRepo = {
+    create: jest.fn((data) => data),
+    save: jest.fn((user) => Promise.resolve({ id, ...user })),
+    find: jest.fn((data?: Record<string, unknown>) => [
+      {
+        ...plainToClass(AuthCode, data),
+        id,
+      },
+    ]),
+    findOne: jest.fn((_id: string) => ({
+      pin,
+      phoneNumber,
+    })),
+  };
+  const mockJwtService = {
+    sign: jest.fn(() => 'ey.test.token'),
+  };
+  const mockSmsService = {
+    sendAuthenticationCode: jest.fn((): void => undefined),
+  };
+  const mockUsersSerivce = {
+    create: jest.fn(),
+    findByPhoneNumber: jest.fn(),
+    findById: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: getRepositoryToken(AuthCode),
+          useValue: mockAuthCodeRepo,
+        },
+        {
+          provide: JwtService,
+          useValue: mockJwtService,
+        },
+        {
+          provide: SmsService,
+          useValue: mockSmsService,
+        },
+        {
+          provide: UsersService,
+          useValue: mockUsersSerivce,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
@@ -14,5 +69,27 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return a reqId after send auth code', async () => {
+    expect(await service.sendAuthenticationCode('+98123456')).toEqual(id);
+  });
+
+  it('should return a phoneNumber after verify authentication code', async () => {
+    expect(await service.verifyAuthenticationCode(idGenerator(), pin)).toEqual(
+      phoneNumber,
+    );
+  });
+
+  it('should call methods on register', async () => {
+    await service.registerUser(phoneNumber);
+    expect(mockUsersSerivce.findByPhoneNumber).toHaveBeenCalled();
+    expect(mockUsersSerivce.create).toHaveBeenCalled();
+  });
+
+  it('should return an access-token', () => {
+    expect(service.login(id)).toEqual({
+      access_token: 'ey.test.token',
+    });
   });
 });
