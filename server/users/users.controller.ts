0495951321c0ab90f06a60edcebd79031d443f4d
Mohammad Reza Hassani <reza_hassani_@outlook.com>
2022-03-04 18:34:32 +0330

    route and functions to create address for user

--- server/users/users.controller.ts
+++ server/users/users.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
 import { Request } from 'express';
+import { AddressService } from '../address/address.service';
+import { AddAddressDto } from '../address/dtos/add-address.dto';
 import { Role } from '../auth/enum/role.enum';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { UserDto } from './dtos/user.dto';
 import { UsersService } from './users.service';
+import { User } from './entities/user.entity';
 import { Serialize } from 'server/common/interceptors/serialize.interceptor';
 
 @Controller('users')
@@ -20,5 +23,11 @@ export class UsersController {
   @Get('profile')
   getProfile(@Req() req: Request) {
     return req.user;
+  }
+
+  @Post('address')
+  addAddress(@Req() req: Request, @Body() data: AddAddressDto) {
+    data.user = req.user as User;
+    return this.usersService.addAddress(req.user.id, data);
   }
 }
--- server/users/users.service.ts
+++ server/users/users.service.ts
@@ -1,12 +1,15 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { AddressService } from '../address/address.service';
+import { AddAddressDto } from '../address/dtos/add-address.dto';
 import { RegisterActionDto } from '../auth/dtos/register-action.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
+    private readonly addressService: AddressService,
     @InjectRepository(User) private readonly usersRepo: Repository<User>,
   ) {}
 
@@ -14,12 +17,40 @@ export class UsersService {
     return this.usersRepo.find();
   }
 
+  async findAndUpdate(id: string, updateDto: any) {
+    try {
+      return await this.usersRepo.save({
+        id,
+        ...updateDto,
+      });
+    } catch (err: any) {
+      throw new InternalServerErrorException(err.message);
+    }
+  }
+
+  async addAddress(id: string, data: AddAddressDto): Promise<User> {
+    const address = await this.addressService.createAddress(data);
+
+    let user = await this.findById(id);
+    if (!user.defaultAddress) {
+      await this.findAndUpdate(id, {
+        defaultAddress: address,
+      });
+
+      user = await this.findById(id);
+    }
+
+    return user;
+  }
+
   async findByPhoneNumber(phoneNumber: string): Promise<User> {
     return (await this.usersRepo.find({ phoneNumber }))[0];
   }
 
   findById(id: string): Promise<User> {
-    return this.usersRepo.findOne(id);
+    return this.usersRepo.findOne(id, {
+      relations: ['defaultAddress', 'addresses'],
+    });
   }
 
   async create({ phoneNumber }: RegisterActionDto): Promise<User> {
